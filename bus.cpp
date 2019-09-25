3ccf57f578e59c703110440d062a720e814d1d94
Erfan Kheyrollahi <ekm507@gmail.com>
2019-09-25 17:05:45 +0330

    add read and write functions for memory.

--- bus.cpp
+++ bus.cpp
@@ -4,3 +4,15 @@ Bus::Bus()
 {
     cpu.connect(this);
 }
+
+uint8_t Bus::read(uint16_t address)
+{
+    if(address >= 0x000 && address <= 0xFFF)
+        return memory[address];
+}
+
+void Bus::write(uint16_t address, uint8_t value)
+{
+    if(address >= 0x000 && address <= 0xFFF )
+        memory[address] = value;
+}
--- bus.h
+++ bus.h
@@ -2,9 +2,23 @@
 
 #include "cpu.h"
 
+#include <cstdint>
+
 class Bus
 {
 public:
+    //define the CPU
     Cpu cpu;
+
+    //Bus constructor
     Bus();
+
+    //main memory.
+    uint8_t memory[4096];
+
+    //read from memory
+    uint8_t read(uint16_t address);
+
+    //write to memory
+    void write(uint16_t address, uint8_t value);
 };
